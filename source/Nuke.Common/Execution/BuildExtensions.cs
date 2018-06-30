1f56b2b22c34718fc817dff6061cb370ae4327c1
Matthias Koch <ithrowexceptions@gmail.com>
2018-06-30 20:46:25 +0200

    Add BuildFactory

--- source/Nuke.Common/Execution/BuildExtensions.cs
+++ source/Nuke.Common/Execution/BuildExtensions.cs
@@ -12,52 +12,6 @@ namespace Nuke.Common.Execution
 {
     internal static class BuildExtensions
     {
-        public static IReadOnlyCollection<TargetDefinition> GetTargetDefinitions<T>(
-            this T build,
-            Expression<Func<T, Target>> defaultTargetExpression)
-            where T : NukeBuild
-        {
-            var defaultTarget = defaultTargetExpression.Compile().Invoke(build);
-            var targetDefinitions = build.GetType()
-                .GetProperties(ReflectionService.Instance)
-                .Where(x => x.PropertyType == typeof(Target))
-                .Select(x => LoadTargetDefinition(build, x)).ToList();
-
-            var nameDictionary = targetDefinitions.ToDictionary(x => x.Name, x => x, StringComparer.OrdinalIgnoreCase);
-            var factoryDictionary = targetDefinitions.ToDictionary(x => x.Factory, x => x);
-
-            foreach (var targetDefinition in targetDefinitions)
-            {
-                var dependencies = GetDependencies(targetDefinition, nameDictionary, factoryDictionary);
-                targetDefinition.TargetDefinitionDependencies.AddRange(dependencies);
-                targetDefinition.IsDefault = targetDefinition.Factory == defaultTarget;
-                
-                targetDefinition.TargetDefinitionDependencies.AddRange(targetDefinition.RunAfterTargets.Select(x => factoryDictionary[x]));
-                targetDefinition.TargetDefinitionDependencies.AddRange(
-                    targetDefinitions.Where(x => x.RunBeforeTargets.Any(y => y == targetDefinition.Factory)));
-            }
-
-            return targetDefinitions;
-        }
-
-        private static TargetDefinition LoadTargetDefinition(NukeBuild build, PropertyInfo property)
-        {
-            var targetFactory = (Target) property.GetValue(build);
-            return TargetDefinition.Create(property.Name, targetFactory);
-        }
-
-        private static IEnumerable<TargetDefinition> GetDependencies(
-            TargetDefinition targetDefinition,
-            IReadOnlyDictionary<string, TargetDefinition> nameDictionary,
-            IReadOnlyDictionary<Target, TargetDefinition> factoryDictionary)
-        {
-            return targetDefinition.NamedDependencies
-                .Select(x => nameDictionary.TryGetValue(x, out var namedTarget)
-                    ? namedTarget
-                    : TargetDefinition.Create(x))
-                .Concat(targetDefinition.FactoryDependencies.Select(x => factoryDictionary[x]));
-        }
-
         public static IReadOnlyCollection<MemberInfo> GetParameterMembers(this NukeBuild build)
         {
             return build.GetInjectionMembers()
